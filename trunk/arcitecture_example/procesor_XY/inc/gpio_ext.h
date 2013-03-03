9c6c6fcf4bbad124e49ffaf9a84a41c67d063ae6
hladiji5@gmail.com <hladiji5@gmail.com@9dc196c8-4030-1678-7862-e956b19f0529>
2013-03-03 18:05:19 +0000

    Nahrany ukazkove hlavickove a C soubory HW vrstvy dac.c adc.c gpio_in.c gpio_out.c gpio_ext.c

---
+++ trunk/arcitecture_example/procesor_XY/inc/gpio_ext.h
@@ -0,0 +1,42 @@
+/**
+  ******************************************************************************
+  * @file    gpio_ext.h
+  * @author  PTO Team
+  * @version V1.0.0
+  * @date    3/3/2013
+  * @brief   This file contains all the functions prototypes for the GPIO_ext firmware 
+  *          library.
+  ******************************************************************************
+*/ 
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __<procesor>_GPIO_EXT_H
+#define __<procesor>_GPIO_EXT_H
+
+/* Includes ------------------------------------------------------------------*/
+/* Zadne includy zde nebudou!!!*/
+
+typedef void (*tick_gpio_ext)(void);
+
+typedef struct
+{  
+  tick_gpio_ext p_GPIO_EXT_tick                 /* Pointer na funkci COUNT_GPIO_Request() */
+}GPIO_EXT_InitTypeDef;
+
+
+enum GPIO_EXT_STATE{
+	GPIO_IDLE = 0,
+	GPIO_RUN,
+	GPIO_ERR
+};
+
+/* GPIO_out_Exported_Functions */
+void GPIO_EXT_init(*GPIO_IN_InitTypeDef); /* Inicializace pomoci deskriptoru */
+GPIO_EXT_STATE GPIO_EXT_get_state();       /* Vrati stav prevodniku */
+//void GPIO_COUNT_IRQ_handler();              
+
+
+
+#endif /*__<procesor>_GPIO_EXT_H */
+
+/************************ END OF FILE *****************************************/
