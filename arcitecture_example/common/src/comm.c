cf0e76f2693be468b76c40dcc7e72bb4fa9a1ce2
hladiji5 <hladiji5@gmail.com>
2013-03-13 12:43:39 +0000

    Doplnena comm knihovna a odstranen problem s includy

--- arcitecture_example/common/src/comm.c
+++ arcitecture_example/common/src/comm.c
@@ -12,7 +12,8 @@
   */ 
 
 /* Includes ------------------------------------------------------------------*/
-#include "<procesor>.h"
+//#include "<procesor>.h"
+#include "stm32f10x.h"
 #include "comm.h"
 #include "uart.h"
 
@@ -21,11 +22,18 @@
 /* Private define ------------------------------------------------------------*/ 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
-COMM_Buffer OutputBuffer;
-COMM_Buffer InputBuffer;
+COMM_Buffer outBuffer;
+COMM_Buffer inBuffer;
 COMM_InitTypeDef COMM_desc;
 COMM_STATE commState;
-struct COMM_CMD commCommand;
+COMM_CMD commCommand;
+UART_InitTypeDef uart;
+
+
+
+uint8_t outBuffField[UART_BUFF_SIZE];
+uint8_t inBuffField[UART_BUFF_SIZE];
+
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 
@@ -34,58 +42,104 @@ struct COMM_CMD commCommand;
  * @param  None
  * @retval None
  */ 
-void COMM_init(struct COMM_InitTypeDef * p_COMM_desc){
-  //TODO
+void COMM_init(COMM_InitTypeDef * p_COMM_desc)
+{
+	COMM_desc = *p_COMM_desc;
+	
+	outBuffer.size = UART_BUFF_SIZE;
+	outBuffer.memory = &outBuffField[0];
+	
+  outBuffer.writePointer = 0;
+  outBuffer.readPointer = 0;
+  outBuffer.state = BUFF_FREE;
+	
+	inBuffer.size = UART_BUFF_SIZE;
+	inBuffer.memory = &inBuffField[0];
+	
+  inBuffer.writePointer = 0;
+  inBuffer.readPointer = 0;
+  inBuffer.state = BUFF_FREE;
+	
+	switch(COMM_desc.speed)
+	{
+		case COMM_HIGH:uart.baudrate = UART_HIGH;break;
+		case COMM_NORMAL:uart.baudrate = UART_NORMAL;break;
+		default:uart.baudrate = UART_SAFE_MODE;
+	}
+	
+	uart.p_inBuffer = &inBuffer;
+	uart.p_outBuffer = &outBuffer;
+	uart.p_COMM_tick = &COMM_tick;
+	
+	UART_init(&uart);
+
 }        
 
 /**
  * @brief  Vrati stav prichoziho bufferu 
  * @param  None
- * @retval None
+ * @retval Buffer state
  */ 
 BUFF_STATE COMM_get_in_buff_state()
 {
-  //TODO
-  return InputBuffer.inPointer;
+  return inBuffer.state;
 };    
 
 /**
  * @brief  Vrati stav odchoziho bufferu
  * @param  None
- * @retval None
+ * @retval Buffer state
  */ 
 BUFF_STATE COMM_get_out_buff_state()
 {
-  //TODO
-  return OutputBuffer.outPointer;
+  return outBuffer.state;
 };     
 
 /**
  * @brief  Vrati pocet volnych byte v prichozim bufferu 
  * @param  None
- * @retval None
+ * @retval uint16_t - free space
  */ 
 uint16_t COMM_get_in_free_space()
 {
-  //TODO
-  return 0;
+
+	uint16_t result;
+	if (inBuffer.readPointer>=inBuffer.writePointer)
+	{
+		result = inBuffer.writePointer-inBuffer.readPointer;
+	}
+	else
+	{
+		result = inBuffer.size-inBuffer.writePointer+inBuffer.readPointer;
+	}
+  return result;
+
 };      
 
 /**
  * @brief  Vrati pocet volnych byte v odchozim bufferu 
  * @param  None
- * @retval None
+ * @retval uint16_t - free space
  */ 
 uint16_t COMM_get_out_free_space()
 {
-  //TODO
-  return 0;
+
+	uint16_t result;
+	if (outBuffer.readPointer>=outBuffer.writePointer)
+	{
+		result = outBuffer.writePointer-outBuffer.readPointer;
+	}
+	else
+	{
+		result = outBuffer.size-outBuffer.writePointer+outBuffer.readPointer;
+	}
+  return result;
 };      
 
 /**
  * @brief  Vrati stav knihovny
  * @param  None
- * @retval None
+ * @retval Comm state
  */ 
 COMM_STATE COMM_get_state()
 {
@@ -96,31 +150,64 @@ COMM_STATE COMM_get_state()
 /**
  * @brief  Ulozi char na vystupni buffer 
  * @param  None
- * @retval None
+ * @retval Buffer state
  */ 
-void COMM_put_char(char c)
+BUFF_STATE COMM_put_char(char chr)
 {
-  //TODO
+
+  uint16_t tmpPointer;
+	
+	outBuffField[outBuffer.writePointer]=chr;
+	tmpPointer=outBuffer.writePointer+1;
+	
+	if (tmpPointer>(outBuffer.size-1))
+	{
+		tmpPointer =0;
+	}
+	
+	
+	if(tmpPointer == outBuffer.readPointer)
+	{
+		outBuffer.state = BUFF_FULL;
+	}
+	else
+	{
+		outBuffer.writePointer=tmpPointer;
+	}
+	return outBuffer.state;
+
 };            
 
 /**
  * @brief  Ulozi znak na vystupni buffer
  * @param  None
- * @retval None
+ * @retval Buffer state
  */ 
-void COMM_put_char(uint8_t c)
+BUFF_STATE COMM_put_uchar(uint8_t chr)
 {
-  //TODO
+  return COMM_put_char(chr);
 };          
 
 /**
  * @brief  Prekopiruje zadanou pamet na vystupni buffer 
  * @param  None
- * @retval None
+ * @retval ok = 0; error = -1
  */ 
-void COMM_send(uint32_t * memory, uint16_t size)
+int8_t COMM_send(uint32_t * memory, uint16_t size)
 {
-  //TODO
+	uint16_t i;
+  if(COMM_get_out_free_space()>=size)
+	{
+		for (i = 0;i<size;i++){
+			COMM_put_char(*memory);
+			memory++;
+		}
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
 };  
 
 /**
@@ -128,10 +215,21 @@ void COMM_send(uint32_t * memory, uint16_t size)
  * @param  None
  * @retval None
  */ 
-uint16_t COMM_read(uint32_t * memory, uint16_t size)
+int16_t COMM_read(uint32_t * memory, uint16_t size)
 {
-  //TODO
-  return 0;
+	uint16_t i=0;;
+	uint8_t chr;
+	while (-1!=chr && i<size)
+	{
+		chr = COMM_read_char();
+		if (-1!=chr){
+			*memory=chr;
+			memory++;
+			i++;
+		}
+	}
+	
+	return i;
 };
 
 /**
@@ -139,10 +237,28 @@ uint16_t COMM_read(uint32_t * memory, uint16_t size)
  * @param  None
  * @retval None
  */ 
-uint8_t COMM_read_char()
+int16_t COMM_read_char()
 {
-  //TODO
-  return 0;
+	
+	int16_t result;
+	
+	if(inBuffer.writePointer == inBuffer.readPointer)
+	{
+		result = -1;
+	}
+	else
+	{
+		result = inBuffField[inBuffer.readPointer];
+		inBuffer.readPointer++;
+	}
+	
+	if(inBuffer.readPointer >= (inBuffer.size))
+	{
+		inBuffer.readPointer = 0; 
+	}
+	
+	
+  return result;
 };            
 
 /**
@@ -150,7 +266,7 @@ uint8_t COMM_read_char()
  * @param  None
  * @retval None
  */ 
-struct COMM_CMD * COMM_get_command()
+COMM_CMD* COMM_get_command()
 {
   //TODO
   return &commCommand;
