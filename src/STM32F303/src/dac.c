ccad876cf8aa29117c0a5063f7717682a47cc417
pto38team <pto38team@9dc196c8-4030-1678-7862-e956b19f0529>
2013-05-04 15:27:50 +0000

    upravena verze dac.c pro stm32F3

--- src/STM32F303/src/dac.c
+++ src/STM32F303/src/dac.c
@@ -25,6 +25,7 @@ bool DACunInitialized = TRUE;
 DAC_STATE dacState = DAC_ERR;
 pto_DAC_InitTypeDef DAC_desc;
 
+
 uint16_t DACbufferPointer = 0;  // Ukazuje do bufferu na vzorek, ktery byl naposled nastaven
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
@@ -35,9 +36,80 @@ uint16_t DACbufferPointer = 0;  // Ukazuje do bufferu na vzorek, ktery byl napos
   * @param  DAC deskriptor
   * @retval None
   */
-void DAC_init(DAC_InitTypeDef * p_DAC_desc)
+void DAC_initialize(pto_DAC_InitTypeDef * p_DAC_desc)
 {
-  //TODO
+  DAC_InitTypeDef            DAC_InitStructure;
+  DMA_InitTypeDef            DMA_InitStructure;
+	GPIO_InitTypeDef 					 GPIO_InitStructure;
+	TIM_TimeBaseInitTypeDef    TIM_TimeBaseStructure;
+	
+	/* DMA2 clock enable (to be used with DAC) */
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA2, ENABLE);
+
+  /* DAC Periph clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC, ENABLE);
+
+  /* GPIOA clock enable */
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+  
+  /* Configure PA.04 (DAC_OUT1) as analog */
+  GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_4;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+	
+	/* TIM2 Periph clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+  
+  /* Time base configuration */
+  TIM_TimeBaseStructInit(&TIM_TimeBaseStructure); 
+  TIM_TimeBaseStructure.TIM_Period = p_DAC_desc->DAC_samplingFrequency;//0x8C9;          
+  TIM_TimeBaseStructure.TIM_Prescaler = 0x00;       
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0x0;    
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  
+  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+  /* TIM2 TRGO selection */
+  TIM_SelectOutputTrigger(TIM2, TIM_TRGOSource_Update);
+  
+  /* TIM2 enable counter */
+  TIM_Cmd(TIM2, ENABLE);
+	
+	/* DAC channel2 Configuration */
+	DAC_DeInit(); 
+	DAC_InitStructure.DAC_Trigger = DAC_Trigger_T2_TRGO;
+	DAC_InitStructure.DAC_WaveGeneration = DAC_WaveGeneration_None;
+	DAC_InitStructure.DAC_LFSRUnmask_TriangleAmplitude = DAC_LFSRUnmask_Bits11_0;
+	DAC_InitStructure.DAC_OutputBuffer = DAC_OutputBuffer_Disable;
+	/* DAC Channel2 Init */
+	DAC_Init(DAC_Channel_1, &DAC_InitStructure);
+	
+	/* Enable DAC Channel2 */
+	DAC_Cmd(DAC_Channel_1, ENABLE);
+	
+	
+	/* DMA2 channel3 configuration */
+	DMA_DeInit(DMA2_Channel3); 
+	DMA_InitStructure.DMA_PeripheralBaseAddr = DAC_DHR12R1_ADDRESS;
+	//DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)&genSine;
+	DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t) p_DAC_desc->p_DAC_memory;
+	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+	DMA_InitStructure.DMA_BufferSize = p_DAC_desc->DAC_memorySize;//32;
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+	DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+	DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+	DMA_Init(DMA2_Channel3, &DMA_InitStructure);
+	
+	/* Enable DMA2 Channel3 */
+	//DMA_Cmd(DMA2_Channel3, ENABLE);
+	
+	/* Enable DMA for DAC Channel2 */
+	//DAC_DMACmd(DAC_Channel_1, ENABLE);
+	dacState = DAC_IDLE;
 }
 
 /**
@@ -48,6 +120,7 @@ void DAC_init(DAC_InitTypeDef * p_DAC_desc)
 void DAC_set_sample(uint16_t sample)
 {
   //TODO
+	dacState = DAC_RUN;
 }        
 
 /**
@@ -57,7 +130,13 @@ void DAC_set_sample(uint16_t sample)
   */
 void DAC_DMA_start()
 {
-  //TODO
+  /* Enable DMA2 Channel3 */
+	DMA_Cmd(DMA2_Channel3, ENABLE);
+	
+	/* Enable DMA for DAC Channel2 */
+	DAC_DMACmd(DAC_Channel_1, ENABLE);
+	
+	dacState = DAC_DMA_RUN;
 }                
 
 /**
@@ -67,7 +146,7 @@ void DAC_DMA_start()
   */
 DAC_STATE DAC_get_state()
 {
-  return DAC_ERR;
+  return dacState;
 }            
 
 /**
