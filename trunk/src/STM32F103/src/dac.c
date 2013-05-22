56c4421351b649dd3ab2563530e87ed694e4db3c
pto38team <pto38team@9dc196c8-4030-1678-7862-e956b19f0529>
2013-05-22 14:24:03 +0000

    komentar k verzi 111 : F103,Commom DAC - pridano, funkcni ale chce debug a odladit pro pc aplikaci ..

--- trunk/src/STM32F103/src/dac.c
+++ trunk/src/STM32F103/src/dac.c
@@ -8,10 +8,10 @@
   *          Moznosti generovani jsou:
   *           - generovani pomoci citace
   *           - generovani pomoci DMA
-  *           - nastaveni jednoho vzorku (vyuzitelne pro DDS)                
-  *       
-  ******************************************************************************  
-  */ 
+  *           - nastaveni jednoho vzorku (vyuzitelne pro DDS)
+  *
+  ******************************************************************************
+  */
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
@@ -23,7 +23,7 @@
 #include "dac.h"
 
 /* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/ 
+/* Private define ------------------------------------------------------------*/
 
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
@@ -42,9 +42,9 @@ u32 TIM_APB1_GetClockFreq(void){
 	RCC_ClocksTypeDef	RCC_Clocks;
 
  	RCC_GetClocksFreq(&RCC_Clocks);
-		
+
 	if(RCC_Clocks.HCLK_Frequency == RCC_Clocks.PCLK1_Frequency){
-		return	RCC_Clocks.PCLK1_Frequency; 
+		return	RCC_Clocks.PCLK1_Frequency;
 	}else{
 		return	2*(RCC_Clocks.PCLK1_Frequency);
 	}
@@ -62,19 +62,19 @@ void DAC_init(PTO_DAC_InitTypeDef * p_DAC_desc)
 	DMA_InitTypeDef            DMA_InitStructure;
 
 //	RCC_ClocksTypeDef	RCC_Clocks;
- 	
+
 	s32 clk_div;
 	u16 prescaler;
 	u16 arr_reg;
-	
+
 //	RCC_GetClocksFreq(&RCC_Clocks);
 
 	lDAC_memorySize = p_DAC_desc->DAC_memorySize;
-	
+
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC, ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-  
+
   	/* Configure PA.04 (DAC_OUT1) as analog */
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
@@ -83,13 +83,13 @@ void DAC_init(PTO_DAC_InitTypeDef * p_DAC_desc)
 
 	/* TIM2 Periph clock enable */
   	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-  
+
   	/* Time base configuration */
-  	TIM_TimeBaseStructInit(&TIM_TimeBaseStructure); 
+  	TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
 	//sampling_frequency = frequency / (psc*arr)
 	//clk_div = psc*arr
 	clk_div = TIM_APB1_GetClockFreq() / p_DAC_desc->DAC_samplingFrequency;
-	
+
 	if(clk_div == 0){
 		//error
 		return;
@@ -100,7 +100,7 @@ void DAC_init(PTO_DAC_InitTypeDef * p_DAC_desc)
 		u32 err_val = 0x0FFFFFFFF;
 		u32 err_min = 0x0FFFFFFFF;
 		u32 err_min_ratio;
-		u16 ratio = clk_div/0x0FFFF; 
+		u16 ratio = clk_div/0x0FFFF;
 		u16 div1;
 		while(err_val != 0){
 			ratio++;
@@ -112,7 +112,7 @@ void DAC_init(PTO_DAC_InitTypeDef * p_DAC_desc)
 				err_min = err_val;
 				err_min_ratio = ratio;
 			}
-		
+
 			if(ratio == 0x0FFFF){
 				//nenasli jsme presnou kombinaci
 				//dosadime nejlepsi zjistenou
@@ -120,37 +120,37 @@ void DAC_init(PTO_DAC_InitTypeDef * p_DAC_desc)
 				ratio = err_min_ratio;
 				err_val = 0;	//konec smycky
 				break;
-			}			
+			}
 		}//while
 		if(ratio > div1){
 			prescaler = div1 -1;
-			arr_reg = ratio - 1;		
+			arr_reg = ratio - 1;
 		}else{
 			prescaler = ratio -1;
 			arr_reg = div1 - 1;
-		}			
+		}
 	}
 
 	TIM_TimeBaseStructure.TIM_Prescaler = prescaler;
-	TIM_TimeBaseStructure.TIM_Period = arr_reg;	
-	TIM_TimeBaseStructure.TIM_ClockDivision = 0x0;    
-  	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  
+	TIM_TimeBaseStructure.TIM_Period = arr_reg;
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0x0;
+  	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
   	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
 
   	/* TIM2 TRGO selection */
   	TIM_SelectOutputTrigger(TIM2, TIM_TRGOSource_Update);
-  
+
   	/* TIM2 enable counter */
   	TIM_Cmd(TIM2, ENABLE);
-	
-	DAC_DeInit(); 
-          
+
+	DAC_DeInit();
+
     /* DAC channel1 Configuration */
     DAC_InitStructure.DAC_Trigger = DAC_Trigger_T2_TRGO;  		//prepis na vystup podle TIM2
     DAC_InitStructure.DAC_OutputBuffer = DAC_OutputBuffer_Enable;
-          
+
 					/* DMA channel2 Configuration */
-   	DMA_DeInit(DMA1_Channel2); 
+   	DMA_DeInit(DMA1_Channel2);
     DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)&DAC->DHR12R1;
     DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)p_DAC_desc->p_DAC_memory;
 	DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
@@ -163,18 +163,18 @@ void DAC_init(PTO_DAC_InitTypeDef * p_DAC_desc)
     DMA_InitStructure.DMA_Priority = DMA_Priority_High;
     DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
     DMA_Init(DMA1_Channel2, &DMA_InitStructure);
-					
+
 			/* Enable DMA1 Channel2 */
     DMA_Cmd(DMA1_Channel2, ENABLE);
-					
+
           	/* DAC Channel1 Init */
     DAC_Init(DAC_Channel_1, &DAC_InitStructure);
 
-          	/* Enable DAC Channel1: Once the DAC channel1 is enabled, PA.04 is 
+          	/* Enable DAC Channel1: Once the DAC channel1 is enabled, PA.04 is
              automatically connected to the DAC converter. */
     DAC_Cmd(DAC_Channel_1, ENABLE);
 	dacState = DAC_IDLE;
-         
+
 }
 
 /**
@@ -184,12 +184,12 @@ void DAC_init(PTO_DAC_InitTypeDef * p_DAC_desc)
   */
 void DAC_set_sample(uint16_t sample)
 {
-	TIM_DMACmd(TIM2, TIM_DMA_Update, DISABLE);					//dma request podle TIM2
+	TIM_DMACmd(TIM2, TIM_DMA_Update, DISABLE);				//dma request podle TIM2
 	//DAC_DHR12R1_ADDRESS
 	DAC->DHR12R1 = sample;
   	dacState = DAC_RUN;
-	
-}        
+
+}
 
 /**
   * @brief  Odstartuje generovani pomoci DMA
@@ -199,10 +199,10 @@ void DAC_set_sample(uint16_t sample)
 void DAC_DMA_start()
 {
    /* Enable DMA for DAC Channel1 */
-    TIM_DMACmd(TIM2, TIM_DMA_Update, ENABLE);					//dma request podle TIM2
+    TIM_DMACmd(TIM2, TIM_DMA_Update, ENABLE);				//dma request podle TIM2
 	dacState = DAC_DMA_RUN;
 }
-                
+
 void DAC_DMA_stop()
 {
    /* Enable DMA for DAC Channel1 */
@@ -217,7 +217,7 @@ void DAC_DMA_stop()
 DAC_STATE DAC_get_state()
 {
   return dacState;
-}            
+}
 
 /**
   * @brief  Vrati index vzorku ktery byl nastaven
@@ -227,7 +227,7 @@ DAC_STATE DAC_get_state()
 uint16_t DAC_get_pointer()
 {
   return DACbufferPointer;
-}              
+}
 
 /**
   * @brief  Nastavi pointer na vzorek o 1 pred ten, ktery se má generovat
@@ -237,15 +237,15 @@ uint16_t DAC_get_pointer()
 int8_t DAC_set_pointer(uint16_t pointer)
 {
   int8_t result = -1;
-  if (pointer < lDAC_memorySize){ /* Kontrola jestli index ukazuje do pole */ 
+  if (pointer < lDAC_memorySize){ /* Kontrola jestli index ukazuje do pole */
     DACbufferPointer = pointer;
     result = 0;
   }
   return result;
-}     
+}
 
 /**
-  * @brief  Obsluha preruseni (bud od casovace pri pravidelnem generovani 
+  * @brief  Obsluha preruseni (bud od casovace pri pravidelnem generovani
   *         nebo od DMA aby se znovu spustilo)
   * @param  None
   * @retval None
@@ -253,7 +253,7 @@ int8_t DAC_set_pointer(uint16_t pointer)
 void DAC_IRQ_handler()
 {
   //zavolani fce v pointeru
-}                
+}
 
 
 /************************ END OF FILE *****************************************/
